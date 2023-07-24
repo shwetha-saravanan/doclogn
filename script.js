04e8633f5dfcb6faf4957cf2a6022467d9ea6b74
Shwetha Saravanan <70532331+shwetha-saravanan@users.noreply.github.com>
2023-07-24 18:47:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
+// JavaScript code for sliding effect and form submissions
 let container = document.getElementById('container');
 
-toggle = () => {
+function toggle() {
   container.classList.toggle('sign-in');
   container.classList.toggle('sign-up');
-};
-
-function toggleSignUp() {
-  let container = document.getElementById('container');
-  container.classList.remove('sign-in');
-  container.classList.add('sign-up');
-};
+}
 
 document.getElementById('signup-btn').addEventListener('click', (event) => {
   event.preventDefault();
-  // Retrieve form values
+  // Retrieve form values for doctor sign-up
   let username = document.getElementById('username').value;
   let email = document.getElementById('email').value;
   let password = document.getElementById('password').value;
-  let docID = document.getElementById('doc-id').value;
-  let hospitalName = document.getElementById('hospital-name').value;
+  let age = document.getElementById('age').value;
+  let height = document.getElementById('Height').value;
+  let bmi = document.getElementById('bmi').value;
+  let isObese = document.getElementById('obese').value;
+  let ethnicity = document.getElementById('ethnicity').value;
+  let hasPreviousGDM = document.getElementById('previous-gdm').value;
 
   // Perform form validation and submission logic here
   // ...
@@ -28,19 +27,20 @@ document.getElementById('signup-btn').addEventListener('click', (event) => {
   console.log('Username:', username);
   console.log('Email:', email);
   console.log('Password:', password);
-  console.log('Doc ID:', docID);
-  console.log('Hospital Name:', hospitalName);
+  console.log('Age:', age);
+  console.log('Height:', height);
+  console.log('BMI:', bmi);
+  console.log('Is Obese:', isObese);
+  console.log('Ethnicity:', ethnicity);
+  console.log('Previous GDM:', hasPreviousGDM);
 
-  // Transition to the sign-in section or perform any desired action
-  toggleSignIn();
-
-  // You can add any other action you want after the sign-up button is clicked.
-  // For example, showing a success message or redirecting to another page.
+  // Transition to the sign-in section
+  toggle();
 });
 
 document.getElementById('signin-btn').addEventListener('click', (event) => {
   event.preventDefault();
-  // Retrieve form values
+  // Retrieve form values for doctor sign-in
   let signinUsername = document.getElementById('signin-username').value;
   let signinPassword = document.getElementById('signin-password').value;
 
@@ -59,6 +59,9 @@ document.getElementById('signin-btn').addEventListener('click', (event) => {
     console.log("Invalid credentials. Please try again.");
   }
 
-  // Transition to the sign-up section or perform any desired action
-  toggleSignUp();
+  // Transition to the sign-up section
+  toggle();
 });
+
+// Initially show the 'sign-up' section
+container.classList.add('sign-up');
