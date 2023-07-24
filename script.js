75cf1040b1543f5ca4e35aefcdddf98aaf15fdaa
Shwetha Saravanan <70532331+shwetha-saravanan@users.noreply.github.com>
2023-07-24 18:16:09 -0400

    Update script.js

--- script.js
+++ script.js
@@ -11,15 +11,19 @@ document.getElementById('signup-btn').addEventListener('click', (event) => {
   let username = document.getElementById('username').value;
   let email = document.getElementById('email').value;
   let password = document.getElementById('password').value;
-  let confirmPassword = document.getElementById('confirm-password').value;
-  let age = document.getElementById('age').value;
-  let bmi = document.getElementById('bmi').value;
-  let ethnicity = document.getElementById('ethnicity').value;
-  let isFirstPregnancy = document.getElementById('first-pregnancy').value === 'Yes';
-  let hasPreviousGDM = document.getElementById('previous-gdm').value === 'Yes';
+  let docID = document.getElementById('doc-id').value;
+  let hospitalName = document.getElementById('hospital-name').value;
 
   // Perform form validation and submission logic here
   // ...
+
+  // For demonstration purposes, let's show the doctor sign-up details in the console
+  console.log('Doctor Sign-up Details:');
+  console.log('Username:', username);
+  console.log('Email:', email);
+  console.log('Password:', password);
+  console.log('Doc ID:', docID);
+  console.log('Hospital Name:', hospitalName);
 
   // Transition to the second page or perform any desired action
   container.classList.remove('sign-up');
@@ -35,9 +39,19 @@ document.getElementById('signin-btn').addEventListener('click', (event) => {
   // Perform sign-in logic here
   // ...
 
+  // For demonstration purposes, let's show the sign-in details in the console
+  console.log('Sign-in Details:');
+  console.log('Username:', signinUsername);
+  console.log('Password:', signinPassword);
+
   // Transition to the second page or perform any desired action
   container.classList.remove('sign-in');
   container.classList.add('sign-up');
+
+  // Redirect to another page after successful login (example: dashboard.html)
+  setTimeout(() => {
+    window.location.href = "dashboard.html";
+  }, 1000); // Redirect after a 1-second delay (for demonstration)
 });
 
 setTimeout(() => {
