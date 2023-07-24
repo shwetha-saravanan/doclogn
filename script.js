426a2bf192a07155d5754df30e55e9353111a21d
Shwetha Saravanan <70532331+shwetha-saravanan@users.noreply.github.com>
2023-07-24 19:04:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,39 @@
 let container = document.getElementById('container');
 
-toggle = () => {
-  container.classList.toggle('sign-in');
-  container.classList.toggle('sign-up');
+const toggleSignup = () => {
+  container.classList.add('slide-signin');
+  container.classList.remove('slide-signup');
+};
+
+const toggleSignin = () => {
+  container.classList.add('slide-signup');
+  container.classList.remove('slide-signin');
 };
 
 document.getElementById('signup-btn').addEventListener('click', (event) => {
   event.preventDefault();
   // Retrieve form values
-  let username = document.getElementById('username').value;
-  let email = document.getElementById('email').value;
-  let password = document.getElementById('password').value;
-  let confirmPassword = document.getElementById('confirm-password').value;
-  let age = document.getElementById('age').value;
-  let bmi = document.getElementById('bmi').value;
-  let ethnicity = document.getElementById('ethnicity').value;
-  let isFirstPregnancy = document.getElementById('first-pregnancy').value === 'Yes';
-  let hasPreviousGDM = document.getElementById('previous-gdm').value === 'Yes';
+  // ... (Your existing form values retrieval code)
 
   // Perform form validation and submission logic here
   // ...
 
-  // Transition to the second page or perform any desired action
-  container.classList.remove('sign-up');
-  container.classList.add('sign-in');
+  // After successful signup, go to the signin page
+  toggleSignin();
 });
 
 document.getElementById('signin-btn').addEventListener('click', (event) => {
   event.preventDefault();
   // Retrieve form values
-  let signinUsername = document.getElementById('signin-username').value;
-  let signinPassword = document.getElementById('signin-password').value;
+  // ... (Your existing form values retrieval code)
 
   // Perform sign-in logic here
   // ...
 
-  // Transition to the second page or perform any desired action
-  container.classList.remove('sign-in');
-  container.classList.add('sign-up');
+  // After successful signin, go to the signup page
+  toggleSignup();
 });
 
 setTimeout(() => {
-  container.classList.add('sign-in');
+  container.classList.add('slide-signin');
 }, 200);
